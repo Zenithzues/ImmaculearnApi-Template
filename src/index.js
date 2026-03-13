f33783cbb2be955aff245737e8c2505032edc26e
wilson <wilsonesmabe2003@gmail.com>
2026-03-13 09:48:38 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -39,8 +39,6 @@ app.use(
         ? cb(null, true)
         : cb(new Error("Not allowed by CORS")),
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
   v1,
 );
